edd6382817f038821755f206a037d0f28f4d8650
Gabriel Mendonça <gabrielmendoncanf@gmail.com>
2016-09-26 00:35:54 -0300

    Agora e possivel passar um parametro para o programa informando o tamanho requerido da senha, o proximo passo e ler alguns padroes para geracao de senha, o tamanho do buffer da senha esta estatico ainda, porem depois pretendo melhorar isso

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,68 @@
 extern crate rand;
 
 use rand::Rng;
+use std::env;
+use std::str::FromStr;
 
-fn main() {
+fn gen_password(size:u8){
 
-	let mut v:[u8;10] = [0;10];
+	if size > 100 || size < 5{
+		panic!("min size for password is 5 and max is 100");
+	}
+
+	let mut v:Vec<u8> = vec![0; 100];
 
 	for i in &mut v{
 		*i = rand::thread_rng().gen_range(33, 125);
 	}
 
-	println!("\n\nYour new password");
+	let mut c:u8 = 0;
 
 	for i in &v{
-		let c:char = *i as char;
-		print!("{}", c);
+
+		if c == size{
+			break;
+		}
+
+		print!("{}", *i as char);
+
+		c += 1;
 	}
+}
 
-	println!("\n\n");
+fn main() {
 
+	let mut c:env::Args = env::args();
+
+	if c.len() == 2{
+
+		let mut size:u8 = 6;
+
+
+		match c.nth(1) {
+		    Some(param) => { 
+		    	println!("{:?}", param);
+		    	match u8::from_str(&param) {
+		    		Ok(s) => {
+		    			size = s;
+		    		},
+		    		Err(_) => {
+		    			println!("parameter is not int");
+		    		}
+		    	}
+		    },
+		    None => { println!("pass password size for params ex: cargo run 10") }
+		}		
+
+
+		if size > 100 || size < 5{
+			println!("min size for password is 5 and max is 100");			
+			std::process::exit(0);
+		}
+
+		print!("\n Your new password:  ");
+		gen_password(size);
+		print!("");
+		println!("\n");
+	}
 }
